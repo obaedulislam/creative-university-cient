22888f1d7e7f59ee553e7f3d883f08c144f2434a
obayedul islam <obaed.cse@gmail.com>
2023-01-02 14:49:54 +0600

    tailwind edited

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,24 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  daisyui: {
-    themes: [
-      {
-        peakbooktheme: {
-          primary: "#02064d",
-          secondary: "#000000",
-          accent: "#388E3C",
-          neutral: "#3d4451",
-          "base-100": "#ffffff",
-        },
-      },
-    ],
-  },
+  // daisyui: {
+  //   themes: [
+  //     {
+  //       peakbooktheme: {
+  //         primary: "#02064d",
+  //         secondary: "#000000",
+  //         accent: "#388E3C",
+  //         neutral: "#3d4451",
+  //         "base-100": "#ffffff",
+  //       },
+  //     },
+  //   ],
+  // },
   theme: {
     extend: {},
+  },
+  daisyui: {
+    theme: false,
   },
   plugins: [require("daisyui")],
 };
