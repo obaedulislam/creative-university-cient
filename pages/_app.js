f7f83331797038b0488e2b1f7967e7fb914a4580
obayedul islam <obaed.cse@gmail.com>
2023-01-02 14:06:33 +0600

    Header & Footer Done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react";
+import Footer from "../Components/Shared/Footer/Footer";
+import Header from "../Components/Shared/Header/Header";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, session }) {
+  return (
+    <div>
+      <SessionProvider session={session}>
+        <Header></Header>
+        <Component {...pageProps} />
+        <Footer></Footer>
+      </SessionProvider>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div></div>
+        <div>
+
+        </div>
       </main>
     </>
   );
